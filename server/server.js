c4e9ad845e8d14cc300786be973bb695c51d566f
Sanjeev <dsrsanjeev28@gmail.com>
2024-04-13 19:05:27 +0530

    Resolved conflicts version 5

--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 const express=require('express')
 const Userapp=require('./Apis/UserApi')
 const Productapp = require('./Apis/ProductApi')
 const cors = require('cors');
 const path=require('path')
 const app=express()
+app.use(allowCors)
 app.use(cors(
   {
     'Access-Control-Allow-Origin':'*'
 
   }
 ));
+
 app.use(express.json())
 require('dotenv').config()
 
