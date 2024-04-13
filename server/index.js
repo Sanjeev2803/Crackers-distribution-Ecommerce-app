b929eb17797aed3daf7f53484927445d15cb64c2
Sanjeev2803 <90560018+Sanjeev2803@users.noreply.github.com>
2024-04-14 00:01:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const cors = require('cors');
 const path=require('path')
 const app=express()
 app.use(cors({
-origin:["https://crackers-distribution-ecommerce-h6tyxzvhn.vercel.app"],
+origin:["https://crackers-distribution-ecommerce-bd57z4uha.vercel.app"],
+
 methods:["POST","GET","PUT","DELETE"],
 credentials:true
 }
