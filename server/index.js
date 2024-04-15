1e9694d55885130c5bbd5383612176396510778c
Sanjeev2803 <90560018+Sanjeev2803@users.noreply.github.com>
2024-04-15 11:48:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,13 +6,21 @@ const Productapp = require('./Apis/ProductApi')
 const cors = require('cors');
 const path=require('path')
 const app=express()
-app.use(cors({
-origin:["https://crackers-distribution-ecommerce-bd57z4uha.vercel.app"],
+// app.use(cors({
+// origin:["https://crackers-distribution-ecommerce-bd57z4uha.vercel.app"],
 
-methods:["POST","GET","PUT","DELETE"],
-credentials:true
-}
-  ))
+// methods:["POST","GET","PUT","DELETE"],
+// credentials:true
+// }
+//   ))
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 // app.use(cors(
 //   {
 //     'Access-Control-Allow-Origin':'*'
