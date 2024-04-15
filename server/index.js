331af0a242706e4b0f6c365602a0c252a1c1245b
Sanjeev2803 <90560018+Sanjeev2803@users.noreply.github.com>
2024-04-15 11:52:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,14 +13,13 @@ const app=express()
 // credentials:true
 // }
 //   ))
- app.use(function (req, res, next) {
-    //Enabling CORS
+app.use(function (req, res, next) {
+    // Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 // app.use(cors(
 //   {
 //     'Access-Control-Allow-Origin':'*'
